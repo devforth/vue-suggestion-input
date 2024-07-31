e839eb3b89b97433296ee6139c369498e0d065d3
Ivan Borshchov <ivan@devforth.io>
2024-07-31 12:19:27 +0300

    cssCodeSplit: false to inject styles

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
     build: {
+        // for styles to work
+        cssCodeSplit: false,
         lib: {
             entry: resolve(__dirname, 'lib/main.ts'),
             name: 'VueSuggestionInput',
